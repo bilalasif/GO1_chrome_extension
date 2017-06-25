79ea85d919020452f94a9846092cda14d45a6625
ZURA\Usman <usman_subhani2020@hotmail.com>
2017-06-25 23:37:00 +0500

    Start Discussion Screen added

--- scripts/Sidebar.js
+++ scripts/Sidebar.js
@@ -4,10 +4,25 @@ if(typeof Sidebar !== 'function'){
 		this.document = document;
 		this.chrome = chrome;
 		this.Vue = Vue;
+		this._initMessageListener();
 		this._initSidebar();
 	}
 	Sidebar.prototype =  {
 		_sidebarHTML: 'html/templates.html',
+		_initMessageListener: function(){
+			var self = this;
+			this.chrome.runtime.onMessage.addListener(function (message, sender, response) {
+				if (!self[message.method]) {
+					throw new Error('Method "' + message.method + '" does not exist');
+				}
+				var tab = sender;
+				message.args = message.args || [];
+				message.args.push(tab);
+				message.args.push(response);
+				self[message.method].apply(self, message.args || []);
+				return true;
+			});
+		},
 		_initSidebar: function(){
 			var self = this;
 			var sidebarContainer = this.document.createElement('div'),
@@ -30,6 +45,13 @@ if(typeof Sidebar !== 'function'){
 			request.onload = onload;
 			request.send();
 		},
+		addPublicNote: function(){
+			this._sidebarApp.discussion
+		},
+
+		addPrivateNote: function(){
+			var self = this;
+		},
 		_initVue: function(sidebarEl){
 			var self = this;
 			this.Vue.component('go-header',{
@@ -39,7 +61,7 @@ if(typeof Sidebar !== 'function'){
 				],
 				methods:{
 					changeScreen: function(el){
-						this.$dispatch('changeFeedScreen', el.currentTarget.id);
+						this.$dispatch('changeFeed', el.currentTarget.id);
 					}
 				}
 			});
@@ -50,11 +72,85 @@ if(typeof Sidebar !== 'function'){
 				]
 			});
 
+			this.Vue.component('go-discussion',{
+				template: '#go-discussion',
+				props:[
+					'discussion'
+				]
+			});
+
+			this.Vue.component('go-start-discussion',{
+				template: '#go-start-discussion',
+				props:[
+					'discussion'
+				],
+				methods:{
+					backToFeed: function(){
+						var vueInstance = this;
+						if(vueInstance.discussion.start.note.active) {
+							//self.chrome.tabs.query({
+							//	active: true,
+							//	currentWindow: true
+							//}, function (tab){
+							//	self.chrome.tabs.sendMessage(tab[0].id, {'method': 'clearPreviousSelection'});
+							//});
+							self.chrome.storage.sync.set({'lastSelectedText': ''}, function () {
+								vueInstance.discussion.start.note.active = false;
+								vueInstance.discussion.start.note.text = '';
+								vueInstance.discussion.start.style.topic = vueInstance.discussion.start.webLink.active ? {height : '40%', top : '38%'} : {height : '60%', top : '18%'};
+								vueInstance.discussion.start.style.weblink =  {height : '20%', top : '18%'};
+								vueInstance.$dispatch('openFeed');
+							});
+						}
+						else{
+							vueInstance.$dispatch('openFeed');
+						}
+					},
+					addDiscussion: function(){
+						var vueInstance = this;
+						var heading = self.document.getElementById('topic');
+						var detail = self.document.getElementById('topicDescription');
+						if(vueInstance.discussion.start.topic.heading.length <= 0){
+							heading.className = 'error';
+						}
+						else if(vueInstance.discussion.start.topic.detail.length <= 0){
+							detail.className = 'error';
+							heading.className = '';
+						}
+						else{
+							self.chrome.storage.sync.set({'lastSelectedText': ''}, function () {
+								vueInstance.$dispatch('uploadDiscussion');
+								detail.className = '';
+								heading.className = '';
+							});
+						}
+					},
+					openDiscussionSettings: function(){
+						alert('settings');
+					},
+					addRemoveWebLink: function(el){
+						this.$dispatch('getMeta', el.currentTarget.checked);
+					}
+				}
+			});
+
+			this.Vue.component('go-continue-discussion',{
+				template: '#go-continue-discussion',
+				props:[
+					'discussion'
+				],
+				methods:{
+					openDiscussionSettings: function(){
+						alert('settings');
+					}
+				}
+			});
+
 			this.Vue.component('go-footer',{
 				template: '#go-footer',
 				methods:{
 					addToDiscussion: function(el){
-						alert('added to discussion');
+						this.$dispatch('startDiscussion');
 					},
 					addToPortal: function(el){
 						alert('added to portal');
@@ -112,15 +208,9 @@ if(typeof Sidebar !== 'function'){
 				props: [
 					'form'
 				],
-				data: function () {
-					return {
-						emailId: null,
-						pwd: null
-					}
-				},
 				methods:{
 					submitLogin: function(){
-						alert("Log in Clicked");
+						this.$dispatch('formSubmit');
 					},
 					gotoRegister: function(){
 						alert("Register Clicked");
@@ -134,9 +224,17 @@ if(typeof Sidebar !== 'function'){
 				el: sidebarEl,
 				template: '#go-root',
 				data: {
-					form: false,
-					feed: {
+					form: {
 						active: true,
+						emailId: null,
+						pwd: null,
+						formError: {
+							active: false,
+							text:''
+						}
+					},
+					feed: {
+						active: false,
 						screens:{
 							chat: {
 								active: true,
@@ -150,20 +248,102 @@ if(typeof Sidebar !== 'function'){
 								active: false
 							}
 						}
+					},
+					discussion:{
+						active: false,
+						start:{
+							active: true,
+							note:{
+								active: false,
+								text: ''
+							},
+							webLink:{
+								active:false,
+								imgSrc:null,
+								url: null,
+								title: null,
+								desc: null
+							},
+							topic:{
+								heading: '',
+								detail: ''
+							},
+							style:{
+								topic: '',
+								weblink: ''
+							}
+						},
+						continue:{
+							active: false
+						}
 					}
 				},
 				methods:{
+					loadInitialScreen: function(){
+						var vueInstance = this;
+						self.chrome.storage.sync.get(null, function(resp){
+							if(resp.loggedIn){
+								if(resp.lastSelectedText && resp.lastSelectedText.length > 0){
+									vueInstance.startDiscussionScreen();
+									vueInstance.discussion.start.note.active = true;
+									vueInstance.discussion.start.note.text = resp.lastSelectedText;
+									vueInstance.discussion.start.style.topic = {height : '50%', top : '28%'};
+									self.chrome.tabs.query({
+										active: true,
+										currentWindow: true
+									}, function (tab){
+										self.chrome.tabs.sendMessage(tab[0].id, {'method': 'highlightSelection'});
+									});
+								}
+								else{
+									vueInstance.openFeedScreen();
+								}
+							}
+						});
+					},
 					adjustPopupDimension: function(){
 						self.document.getElementsByTagName("html")[0].style.height="600px";
-						if(this.form) {
+						if(this.form.active) {
 							self.document.getElementsByTagName("html")[0].style.width = "300px";
 						}
-						else if(this.feed.active){
+						else {
 							self.document.getElementsByTagName("html")[0].style.width = "450px";
 						}
+					},
+					changeFeedScreen: function(screenId){
+						var vueInstance = this;
+						if(vueInstance.feed){
+							for(var screen in vueInstance.feed.screens){
+								if(vueInstance.feed.screens.hasOwnProperty(screen)) {
+									vueInstance.feed.screens[screen].active = screen === screenId;
+								}
+							}
+						}
+					},
+					openFeedScreen : function(){
+						this.discussion.active= false;
+						this.form.active= false;
+						this.feed.active= true;
+						this.changeFeedScreen('chat');
+						this.adjustPopupDimension();
+					},
+					startDiscussionScreen : function(){
+						this.feed.active = false;
+						this.discussion.active = this.discussion.start.active = true;
+						this.discussion.start.webLink.active = false;
+						this.form.active= false;
+						this.adjustPopupDimension();
 					}
 				},
 				events:{
+					uploadDiscussion: function(){
+						this.openFeedScreen();		//upload data to api
+						console.log("Heading: "+(this.discussion.start.topic.heading || 'Empty'));
+						console.log("Detail: "+(this.discussion.start.topic.detail || 'Empty'));
+						console.log("Link: "+(this.discussion.start.webLink.url || 'Empty'));
+						console.log("Note: "+(this.discussion.start.note.text || 'Empty'));
+						alert('note added');
+					},
 					updateHighestRated: function(){
 						//update from api
 						this.feed.screens.chat.chatList .push({
@@ -211,19 +391,68 @@ if(typeof Sidebar !== 'function'){
 							}
 						}];
 					},
-					changeFeedScreen: function(screenId){
+					changeFeed: function(screenId){
+						this.changeFeedScreen(screenId);
+					},
+					formSubmit: function(){
 						var vueInstance = this;
-						if(vueInstance.feed){
-							for(var screen in vueInstance.feed.screens){
-								if(vueInstance.feed.screens.hasOwnProperty(screen)) {
-									vueInstance.feed.screens[screen].active = screen === screenId;
+						this.form.formError.active = !this.form.emailId || this.form.emailId.indexOf('@') === -1 || !this.form.pwd;
+						if(!this.form.emailId && !this.form.pwd){
+							this.form.formError.text = "Please enter email and password"
+						}
+						else if(!this.form.emailId || this.form.emailId.indexOf('@') === -1){
+							this.form.formError.text = "Please enter valid email address"
+						}
+						else if(!this.form.pwd){
+							this.form.formError.text = "Please enter password"
+						}
+						else{
+							//send to api
+							//form submitted
+							//incase of success
+							self.chrome.storage.sync.set({"loggedIn": true});
+							self.chrome.tabs.query({}, function(tabs){
+								for(var i =0; i<tabs.length; i++){
+									self.chrome.tabs.sendMessage(tabs[i].id, {"method":"initHighlighter"});
 								}
-							}
+								vueInstance.openFeedScreen();
+							});
+						}
+					},
+					startDiscussion: function(){
+						this.startDiscussionScreen();
+					},
+					openFeed: function(){
+						this.openFeedScreen();
+					},
+					getMeta: function(check){
+						var vueInstance = this;
+						if(check){
+							self.chrome.tabs.query({
+								active: true,
+								currentWindow: true
+							}, function (tab){
+								self.chrome.tabs.sendMessage(tab[0].id, {'method': 'getMetaContent'}, function(resp){
+									console.log(resp);
+									vueInstance.discussion.start.webLink.url = resp.url;
+									vueInstance.discussion.start.webLink.imgSrc = resp.imgSrc;
+									vueInstance.discussion.start.webLink.desc = resp.desc;
+									vueInstance.discussion.start.webLink.title = resp.title;
+									vueInstance.discussion.start.webLink.active = true;
+									vueInstance.discussion.start.style.topic = vueInstance.discussion.start.note.active ? {height : '28%', top : '50%'} : {height : '40%', top : '38%'};
+									vueInstance.discussion.start.style.weblink = vueInstance.discussion.start.note.active ? {height : '20%', top : '28%'} : {height : '20%', top : '18%'};
+								});
+							});
+						}
+						else{
+							vueInstance.discussion.start.webLink.active = false;
+							vueInstance.discussion.start.style.topic = vueInstance.discussion.start.note.active ? {height : '50%', top : '28%'} : {height : '60%', top : '18%'};
 						}
 					}
 				},
 				ready: function(){
 					this.adjustPopupDimension();
+					this.loadInitialScreen();
 				}
 			});
 		}
---
+++ scripts/contentApp.js
@@ -0,0 +1,248 @@
+if(typeof ContentApp !== 'function'){
+	function ContentApp(window, document,chrome){
+		var self = this;
+		this.chrome = chrome;
+		this.window = window;
+		this.document = document;
+		this._initMessageListener();
+		this.chrome.storage.sync.get({"loggedIn":false}, function (resp) {
+			if(resp.loggedIn){
+				self._initHighlightHandler();
+			}
+		});
+	}
+	ContentApp.prototype = {
+		createNoteSpan: null,
+		rangeNodes: [],
+		_initMessageListener: function(){
+			var self = this;
+			this.chrome.runtime.onMessage.addListener(function (message, sender, response) {
+				if (!self[message.method]) {
+					throw new Error('Method "' + message.method + '" does not exist');
+				}
+				var tab = sender;
+				message.args = message.args || [];
+				message.args.push(tab);
+				message.args.push(response);
+				self[message.method].apply(self, message.args || []);
+				return true;
+			});
+		},
+		initHighlighter: function(){
+			this._initHighlightHandler();
+		},
+		findAncestor: function (el, target) {
+		while ((el = el.parentElement) && !(el===target));
+		return el;
+		},
+		_initHighlightHandler: function(){
+			var self = this;
+			this.document.onmouseup = function (e) {
+				if ( !self.createNoteSpan || (self.createNoteSpan && !self.findAncestor(e.target, self.createNoteSpan))) {
+					var selectedTextObject;
+					var selectedText;
+					if (self.window.getSelection) {
+						selectedTextObject = self.window.getSelection();
+						selectedText = selectedTextObject.toString();
+					} else if (self.document.selection && self.document.selection.type != "Control") {
+						selectedTextObject = self.document.selection.createRange();
+						selectedText = selectedTextObject.text;
+					}
+					self.clearPreviousSelection();
+					if (selectedText.length > 0) {
+						console.log("X: " + e.pageX + " Y: " + e.pageY);
+						self._createNoteTooltip(selectedTextObject, e.pageX, e.pageY);
+					}
+				}
+			};
+		},
+		highlightSelection: function(){
+			var self = this;
+			if(self.createNoteSpan) {
+				self.createNoteSpan.parentElement.removeChild(self.createNoteSpan);
+				self.createNoteSpan = null;
+			}
+			if(self.rangeNodes.length > 0){
+				for(var i=0; i < self.rangeNodes.length; i++){
+					self.rangeNodes[i].removeEventListener("mouseenter", self._addOpacity.bind(self.createNoteSpan));
+					self.rangeNodes[i].removeEventListener("mouseleave", self._removeOpacity.bind(self.createNoteSpan));
+					self.rangeNodes[i].className += ' highlight';
+				}
+				self.rangeNodes = [];
+			}
+		},
+		_addOpacity: function(event){
+			this.style.opacity = '1';
+		},
+		_removeOpacity: function(event){
+			this.style.opacity = '0';
+		},
+		_createButtons: function(selectedText){
+			var self = this;
+			var buttonWrapper = this.document.createElement('div');
+			var createPublicNote = this.document.createElement('div');
+			var createPrivateNote = this.document.createElement('div');
+			var publicNoteimg = this.document.createElement('img');
+			var privateNoteimg = this.document.createElement('img');
+			var publicNote = this.document.createElement('p');
+			var privateNote = this.document.createElement('p');
+			publicNoteimg.src = this.chrome.runtime.getURL('/Resources/images/createNote.PNG');
+			privateNoteimg.src = this.chrome.runtime.getURL('/Resources/images/createPrivateNote.PNG');
+			buttonWrapper.className = 'noteWrapper';
+			createPublicNote.className = 'createNoteButton';
+			createPublicNote.className += ' public';
+			publicNote.innerText = 'Create Note';
+			createPrivateNote.className = 'createNoteButton';
+			createPrivateNote.className += ' private';
+			privateNote.innerText = 'Private Note';
+			createPublicNote.appendChild(publicNoteimg);
+			createPublicNote.appendChild(publicNote);
+			createPublicNote.addEventListener('click', function(event){
+				self.chrome.storage.sync.set({'lastSelectedText':selectedText});
+			});
+			createPrivateNote.appendChild(privateNoteimg);
+			createPrivateNote.appendChild(privateNote);
+			createPrivateNote.addEventListener('click', function(event){
+				self.chrome.storage.sync.set({'lastSelectedText':selectedText});
+			});
+			buttonWrapper.appendChild(createPublicNote);
+			buttonWrapper.appendChild(createPrivateNote);
+			this.createNoteSpan.appendChild(buttonWrapper);
+		},
+		clearPreviousSelection: function(){
+			if(this.createNoteSpan) {
+				this.createNoteSpan.parentElement.removeChild(this.createNoteSpan);
+				this.createNoteSpan = null;
+			}
+			if(this.rangeNodes.length > 0){
+				for(var i=0; i < this.rangeNodes.length; i++){
+					this.rangeNodes[i].removeEventListener("mouseenter", this._addOpacity.bind(this.createNoteSpan));
+					this.rangeNodes[i].removeEventListener("mouseleave", this._removeOpacity.bind(this.createNoteSpan));
+					this.rangeNodes[i].className = '';
+				}
+				this.rangeNodes = [];
+			}
+			this.chrome.storage.sync.set({'lastSelectedText':''});
+		},
+		_createNoteTooltip: function(selectedTextObject, locX, locY){
+			var self = this;
+			this.createNoteSpan = this.document.createElement('span');
+			this.createNoteSpan.className = 'createNote';
+			this._createButtons(selectedTextObject.toString());
+			this.createNoteSpan.style.top = (locY-120)+'px';
+			this.createNoteSpan.style.left = (locX-120)+'px';
+			this.createNoteSpan.addEventListener("mouseenter", self._addOpacity.bind(self.createNoteSpan));
+			this.createNoteSpan.addEventListener("mouseleave", self._removeOpacity.bind(self.createNoteSpan));
+			this._highlightText(selectedTextObject);
+			this.document.body.appendChild(this.createNoteSpan);
+		},
+		_highlightText: function(selectedTextObject){
+			var safeRanges = this._getSafeRanges(selectedTextObject.getRangeAt(0));
+			for (var i = 0; i < safeRanges.length; i++) {
+				this.rangeNodes.push(this._highlightRange(safeRanges[i]));
+			}
+		},
+		_highlightRange: function (range) {
+			var self = this;
+			var newNode = this.document.createElement("span");
+			newNode.className = 'selectedText';
+			newNode.addEventListener("mouseenter", self._addOpacity.bind(self.createNoteSpan));
+			newNode.addEventListener("mouseleave", self._removeOpacity.bind(self.createNoteSpan));
+			range.surroundContents(newNode);
+			return newNode;
+		},
+		_getSafeRanges: function(dangerous) {
+			var self = this;
+			var a = dangerous.commonAncestorContainer;
+			// Starts -- Work inward from the start, selecting the largest safe range
+			var s = new Array(0), rs = new Array(0);
+			if (dangerous.startContainer != a)
+				for(var i = dangerous.startContainer; i != a; i = i.parentNode)
+					s.push(i)
+					;
+			if (0 < s.length) for(var i = 0; i < s.length; i++) {
+				var xs = self.document.createRange();
+				if (i) {
+					xs.setStartAfter(s[i-1]);
+					xs.setEndAfter(s[i].lastChild);
+				}
+				else {
+					xs.setStart(s[i], dangerous.startOffset);
+					xs.setEndAfter(
+							(s[i].nodeType == Node.TEXT_NODE)
+									? s[i] : s[i].lastChild
+					);
+				}
+				rs.push(xs);
+			}
+
+			// Ends -- basically the same code reversed
+			var e = new Array(0), re = new Array(0);
+			if (dangerous.endContainer != a)
+				for(var i = dangerous.endContainer; i != a; i = i.parentNode)
+					e.push(i)
+					;
+			if (0 < e.length) for(var i = 0; i < e.length; i++) {
+				var xe = self.document.createRange();
+				if (i) {
+					xe.setStartBefore(e[i].firstChild);
+					xe.setEndBefore(e[i-1]);
+				}
+				else {
+					xe.setStartBefore(
+							(e[i].nodeType == Node.TEXT_NODE)
+									? e[i] : e[i].firstChild
+					);
+					xe.setEnd(e[i], dangerous.endOffset);
+				}
+				re.unshift(xe);
+			}
+
+			// Middle -- the uncaptured middle
+			if ((0 < s.length) && (0 < e.length)) {
+				var xm = self.document.createRange();
+				xm.setStartAfter(s[s.length - 1]);
+				xm.setEndBefore(e[e.length - 1]);
+			}
+			else {
+				return [dangerous];
+			}
+
+			// Concat
+			rs.push(xm);
+			var response = rs.concat(re);
+
+			// Send to Console
+			return response;
+		},
+		getMetaContent: function(){
+			var self = this;
+			var response = arguments[1];
+			var imgSrc = '';
+			var description = '';
+			var title = '';
+			var metaTags = self.document.getElementsByTagName('meta');
+			for (var i=0; i<metaTags.length; i++) {
+				if (metaTags[i].getAttribute("property") == "og:image") {
+					imgSrc = metaTags[i].getAttribute("content");
+				}
+				if (metaTags[i].getAttribute("property") == "og:description") {
+					description = metaTags[i].getAttribute("content");
+				}
+				if (metaTags[i].getAttribute("property") == "og:title") {
+					title = metaTags[i].getAttribute("content");
+				}
+			}
+			response({
+				url: self.window.location.href,
+				imgSrc:imgSrc||self.document.getElementsByTagName('img')[0].src,
+				desc:description||decodeURIComponent(self.document.getElementsByTagName('title')[0].innerText),
+				title:title||decodeURIComponent(self.document.getElementsByTagName('title')[0].innerText)
+			});
+		}
+	}
+}
+if(!window.GO){
+	window.GO = {};
+}
+window.GO.backgroundApp = new ContentApp (window, document,chrome);
